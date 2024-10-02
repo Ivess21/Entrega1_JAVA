95743253ead8f69f06c48999cba9655f3b8adaca
Ives Acosta <ivesdev21@gmail.com>
2024-10-01 21:49:41 -0300

    Primera entrega - Acosta Ives

---
+++ src/test/java/P/entrega/Acosta/Ives/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package P.entrega.Acosta.Ives.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
